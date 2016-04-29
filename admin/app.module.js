ea52ac61344041fdd639762fdc20e638edac350f
maxamed daahir <Ducale@Own.(none)>
2016-04-29 10:50:07 +0300

    added toolbar with tabs

--- admin/app.module.js
+++ admin/app.module.js
@@ -5,15 +5,27 @@
     .module("unoteAdmin", ['ngMaterial','ui.router'])
 
     .config(function($stateProvider, $urlRouterProvider){
+
+      $urlRouterProvider.otherwise('/home');
+
       $stateProvider
+        .state('home',{
+          url: '/home',
+          templateUrl:'partials/home.html'
+        })
         .state('home.students',{
-          url: 'students',
-          template: 'TODO'
+          url: '/students',
+          templateUrl: 'partials/students.html'
         })
         .state('home.teachers',{
-          url: 'teachers',
-          template: 'TODO'
+          url: '/teachers',
+          templateUrl: 'partials/teachers.html'
         })
     })
+
+    .config(['$mdIconProvider', function($mdIconProvider){
+      $mdIconProvider.
+        icon("menu", "/css/svg/ic_menu_white_24px.svg", 24)
+    }])
 
 })();
