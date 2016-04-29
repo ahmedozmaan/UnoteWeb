da59233ff4028cd99f80dd08c39f5bfcd0fa2ab0
maxamed daahir <Ducale@Own.(none)>
2016-04-29 09:56:44 +0300

    configured ui-router

--- admin/app.module.js
+++ admin/app.module.js
@@ -2,5 +2,18 @@
   'use strict';
 
   angular
-    .module("unoteAdmin", ['ngMaterial'])
+    .module("unoteAdmin", ['ngMaterial','ui.router'])
+
+    .config(function($stateProvider, $urlRouterProvider){
+      $stateProvider
+        .state('home.students',{
+          url: 'students',
+          template: 'TODO'
+        })
+        .state('home.teachers',{
+          url: 'teachers',
+          template: 'TODO'
+        })
+    })
+
 })();
