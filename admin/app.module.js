da679d8d1d18dafb6b51fb3c69163b3995abe6e0
maxamed daahir <Ducale@Own.(none)>
2016-06-18 07:09:52 +0300

    added edit icon

--- admin/app.module.js
+++ admin/app.module.js
@@ -12,8 +12,7 @@
       .state('home', {
         url: '/home',
         templateUrl: 'home/partials/home.html',
-        controllerAs: "vm",
-        controller: 'usersCtrl'
+        controllerAs: "vm"
       })
       .state('home.students', {
         url: '/students',
@@ -48,6 +47,7 @@
       .icon("add", "/css/svg/ic_add_white_24px.svg", 24)
       .icon("logout", "/css/svg/ic_power_settings_black_24px.svg", 24)
       .icon("delete", "css/svg/ic_delete_black_24px.svg", 24)
+      .icon("edit", "css/svg/ic_mode_edit_black_24px.svg", 24)
   }])
 
   .config(['$mdThemingProvider', function($mdThemingProvider) {
